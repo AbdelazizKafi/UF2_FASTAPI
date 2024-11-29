8237034712dea914edd09b1e901c4b19729cdfcb
AbdelazizKafi <2024_abdelaziz.kafi@iticbcn.cat>
2024-11-30 00:07:19 +0100

    commit exercici 6 error 404

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,6 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Item(BaseModel):
+    id: int
+    name: str
+    description: str
+    price: float
+    in_stock: bool
+    discount: Optional[float] = None  
+
+fake_db = {
+    1: {"name": "Element A", "description": "Descripció A", "price": 100.0, "in_stock": True, "discount": 10.0},
+    2: {"name": "Element B", "description": "Descripció B", "price": 200.0, "in_stock": False, "discount": None},
+    3: {"name": "Element C", "description": "Descripció C", "price": 300.0, "in_stock": True, "discount": 5.0}
+}
 
 @app.get("/")
 def read_root():
@@ -8,14 +24,18 @@ def read_root():
 
 @app.get("/item/{item_id}")
 def get_item(item_id: int):
-    return {"message": f"S'ha introduït l'element amb ID: {item_id}"}
+    
+    if item_id not in fake_db:
+        
+        raise HTTPException(status_code=404, detail=f"Item amb ID {item_id} no trobat")
+    return fake_db[item_id]
 
 @app.get("/items/")
 def get_items():
     return {"items": [
-        {"id": 1, "name": "Usuari A"},
-        {"id": 2, "name": "Usuari B"},
-        {"id": 3, "name": "Usuari C"},
+        {"id": 1, "name": "Usuari A", "description": "Descripció A", "preu": 100.0, "in_stock": True},
+        {"id": 2, "name": "Usuari B", "description": "Descripció B", "preu": 200.0, "in_stock": False},
+        {"id": 3, "name": "Usuari C", "description": "Descripció C", "preu": 300.0, "in_stock": True},
     ]}
 
 @app.get("/user/{username}")
@@ -26,3 +46,8 @@ def get_user(username: str):
 def calculate(a: int, b: int):
     result = a + b
     return {"operation": "suma", "a": a, "b": b, "result": result}
+
+@app.post("/item/")
+def create_item(item: Item):
+    fake_db[item.id] = item.dict()  
+    return {"message": f"Element creat: {item.name} amb ID {item.id}"}
